d81fb52fb78b4da94986f7be2dac3e347f9018f7
Quin Marilyn <quin.marilyn05@gmail.com>
2021-09-30 18:25:32 -0600

    Bump versoin.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-nuitka --windows-company-name=Accessware --windows-product-name=Lookup --windows-file-version=0.20 --windows-product-version=0.20 --windows-file-description=Lookup --standalone --python-flag=no_site --windows-disable-console --remove-output lookup.pyw
+nuitka --windows-company-name=Accessware --windows-product-name=Lookup --windows-file-version=0.11 --windows-product-version=0.11 --windows-file-description=Lookup --standalone --python-flag=no_site --windows-disable-console --remove-output lookup.pyw
 pause > nul
