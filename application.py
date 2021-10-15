1a6665c4475134adcae85866e97f4ece8b30d243
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-14 18:57:07 -0600

    Bump version.

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ import os
 class Application:
 	def __init__(self):
 		self.name = "Lookup"
-		self.version = "0.21"
+		self.version = "0.30"
 		self.running = False
 		self.config = None
 		self.load_config()
