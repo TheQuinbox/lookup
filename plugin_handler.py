dd438011e7d98e1951446ab18545088fc2c18d24
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-22 19:05:26 -0600

    Fixed imports.

--- plugin_handler.py
+++ plugin_handler.py
@@ -1,9 +1,10 @@
-import yapsy
+from yapsy.IPlugin import IPlugin
+from yapsy.PluginManager import PluginManager
 
-class Plugin(yapsy.IPlugin.IPlugin):
+class Plugin(IPlugin):
 	def get_text(self):
 		pass
 
 def Get_plugin_manager():
-	manager = yapsy.PluginManager.PluginManager(directories_list=["plugins", "plugin"], plugin_info_ext="lookup")
+	manager = PluginManager(directories_list=["plugins", "plugin"], plugin_info_ext="lookup")
 	return manager
